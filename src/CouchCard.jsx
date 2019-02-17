71bcc52fa609f18fdc481e0246c97dd50f5a3d82
Marvin Estrada <marvinestrada2@gmail.com>
2019-02-16 18:54:41 -0600

    Cards Work

---
+++ src/CouchCard.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+class CouchCard extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render () {
+        return (
+            <div>
+                <Card>
+                    <CardImg src={this.props.couch.image}/>
+                    <CardBody>
+                    <CardTitle>{this.props.couch.name}</CardTitle>
+                    <CardSubtitle>{this.props.couch.color}</CardSubtitle>
+                    <CardText>{this.props.couch.price}</CardText>
+                    <Button>Buy</Button>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+    }
+}
+
+export default CouchCard;
--- src/PicLayout.js
+++ src/PicLayout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import CouchCard from './CouchCard';
 import Couch1 from './Couch1.jpg';
 import Couch2 from './Couch2.jpg';
 import Couch3 from './Couch4.jpg';
@@ -8,31 +9,49 @@ import Couch4 from './Couch3.jpg';
 class PicLayout extends Component {
     constructor() {
         super();
+        this.state = {
+            couches: [
+                {
+                    image: Couch1,
+                    name: "Couch#1",
+                    color: "Black",
+                    price: "Price: $500"
+                },
+                {
+                    image: Couch2,
+                    name: "Couch#2",
+                    color: "Brown",
+                    price: "Price: $700"
+                },
+                {
+                    image: Couch3,
+                    name: "Couch#2",
+                    color: "Brown",
+                    price: "Price: $700"
+                },
+                {
+                    image: Couch4,
+                    name: "Couch#2",
+                    color: "Brown",
+                    price: "Price: $700"
+                }
+            ]
+        }
     }
     render () {
+        let couchCard = this.state.couches.map(couch => {
+            return (
+                <Col>
+                    <center>
+                        <CouchCard couch={couch}/>
+                    </center>
+                </Col>
+            )
+        })
         return (
             <Container fluid>
                 <Row>
-                    <Col>
-                        <center>
-                            <img src={Couch1} style={{width:250,height:200}}/>
-                        </center>
-                    </Col>
-                    <Col>
-                        <center>
-                            <img src={Couch2} style={{width:250,height:200}}/>
-                        </center>
-                    </Col>
-                    <Col>
-                        <center>
-                            <img src={Couch3} style={{width:250,height:200}}/>
-                        </center>
-                    </Col>
-                    <Col>
-                        <center>
-                            <img src={Couch4} style={{width:250,height:200}}/>
-                        </center>
-                    </Col>
+                    {couchCard}
                 </Row>
             </Container>
         )
