e44f572aede8a9e9b791fd47b61530b2414765a8
Marvin Estrada <marvinestrada2@gmail.com>
2019-02-18 09:24:43 -0600

    Fixed Logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
 import Navbar from './Navbar';
 import logo from './LogoCouchUp.jpg';
 import PicLayout from './PicLayout';
@@ -12,7 +13,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <center><img src={logo} style={{width:450,height:150}}/></center>
+      <Container fluid>
+                <Row>
+                  <center><img src={logo} style={{width:450,height:150}}/></center>
+                </Row>
+            </Container>
+        {/*<center><img src={logo} style={{width:450,height:150}}/></center>*/}
         <Navbar/>
         <center><SearchBar/></center>
         <center><Carousel/></center>
