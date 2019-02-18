4ac743b41bc6b951c3d63bcfd94571b21d4db79e
Marvin Estrada <marvinestrada2@gmail.com>
2019-02-18 08:32:58 -0600

    Domain ready

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if (window.location.protocol !== "https:") {
+    window.location =
+      "https://" + window.location.hostname + window.location.pathname + window.location.hash;
+}
