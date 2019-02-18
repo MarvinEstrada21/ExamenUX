27c71915b4b76349e1a5e66420ccb3f831bb4cb5
Marvin Estrada <marvinestrada2@gmail.com>
2019-02-17 23:49:17 -0600

    Search bar fixed

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { InputGroup, InputGroupAddon, Button, Input } from 'reactstrap';
 
 const Example = (props) => {
-  return (
-    <div>
-      <InputGroup>
-        <Input />
-        <InputGroupAddon addonType="append">
-          <Button color="secondary">Search</Button>
-        </InputGroupAddon>
-      </InputGroup>
-    </div>
-  );
+    return (
+        <div>
+            <InputGroup>
+                <Input placeholder="Insert a color" />
+                <InputGroupAddon addonType="append">
+                    <Button color="secondary">Search</Button>
+                </InputGroupAddon>
+            </InputGroup>
+        </div>
+    );
 };
 
 export default Example;
